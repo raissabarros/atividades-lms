75afebbea7f7ccfa7e3a814ecebf60686287640a
raissabarros <raissabarros@alu.ufc.br>
2017-10-15 16:14:19 -0300

    Update ativ05.js

--- Atividade 05/ativ05.js
+++ Atividade 05/ativ05.js
@@ -2,7 +2,19 @@
  * Created by raissa on 10/10/17.
  */
 
-let conso = "teste de console"
+/* let conso = "teste de console"
 
     console.log(conso);
+*/
+
+function abrirMenu(){
+    document.getElementById("menulateral").style.width = "200px";
+    document.getElementById("header").style.marginLeft = "200px";
+}
+
+function fecharMenu() {
+    document.getElementById("menulateral").style.width = "0";
+    document.getElementById("header").style.marginLeft = "0";
+}
+
 
