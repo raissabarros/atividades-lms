a8552385f0d60442cf6afee256304f105cd829f1
raissabarros <raissabarros@alu.ufc.br>
2017-10-15 18:03:44 -0300

    Update ativ05.js

--- Atividade 05/ativ05.js
+++ Atividade 05/ativ05.js
@@ -8,13 +8,29 @@
 */
 
 function abrirMenu(){
-    document.getElementById("menulateral").style.width = "200px";
-    document.getElementById("header").style.marginLeft = "200px";
+    document.getElementById("menu").style.width = "150px";
+    document.getElementById("cabecalho").style.marginLeft = "150px";
+    document.getElementById("main").style.marginLeft = "150px";
 }
 
 function fecharMenu() {
-    document.getElementById("menulateral").style.width = "0";
-    document.getElementById("header").style.marginLeft = "0";
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("cabecalho").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
 
 
+//o valor da checkbox indica se o menu está visível
+//  se a checkbox está marcada (checked é true), o menu está visível
+//  se a checkbox não está marcada (checked é false), o menu está escondido
+//obs: o valor da checkbox começa como false
+function alternarMenu(){
+    var x = document.getElementById("check");   //x é a nossa checkbox
+    if (x.checked) {                            //se a checkbox está marcada (o menu está aberto) então
+        fecharMenu();                           //  fechamos o menu
+        x.checked = false;                      //  desmarcamos a checkbox (o menu agora está fechado
+    } else {                                    //senão (se a checkbox não está marcada, o menu está fechado) então
+        abrirMenu();                            //  abrimos o menu
+        x.checked = true;                       //  marcamos a checkbox (o menu agora está aberto)s
+    }
+}
