ce76e319282d5cc6b6bd7551b0c9427b6a810687
raissabarros <raissabarros@alu.ufc.br>
2017-11-04 17:57:58 -0300

    Add files via upload

---
+++ Trabalho 02/trab02.js
@@ -0,0 +1,97 @@
+/**
+ * Created by raissa on 31/10/17.
+ */
+
+/*function entrar(){
+    document.getElementsById("conversas").style.display = "block";
+}
+
+var entrar = document.getElementsByClassName("conversas");
+var i;
+
+for (i = 0; i < entrar.length; i++) {
+   entar[i].onclick = function(){
+       this.classList.toggle("active");
+
+       var conversas = this.nextElementSibling;
+
+       if (conversas.style.display){
+           conversas.style.display = block;
+       } else {
+         conversas.style.display = none;
+       }
+   }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+let chat = [
+    {
+        usuario: “mamae”,
+        imagem: "imagens/avatar02.png",
+        mensagens: [
+            {
+                usuario: “mamae”,
+                texto: “Bom dia, Baby! Tudo bem?”
+            },
+            {
+                usuario: “raissa”,
+                texto: “Tudo, daí”
+            },
+            {
+                usuario: “mamae”,
+                texto: “Que bom”
+            }
+        ]
+    },
+    {
+        usuario: “filipe”,
+        imagem: "imagens/avatar03.png",
+        mensagens: [
+            {
+                usuario: “filipe”,
+                texto: “Olarrr!”
+            },
+            {
+                usuario: “raissa”,
+                texto: “olarr”
+            },
+            {
+                usuario: “filipe”,
+                texto: “Te amo! Boa aula!”
+            }
+        ]
+    },
+    {
+        usuario: “marilia”,
+        imagem: "imagens/avatar01.png",
+        mensagens: [
+            {
+                usuario: “raissa”,
+                texto: “Miga, vamo no Submarine?”
+            },
+            {
+                usuario: “marilia”,
+                texto: “ow, miga, tô sem dinheiro =(”
+            },
+            {
+                usuario: “raissa”,
+                texto: “ow, bicha, queria ir. Eu pago, bora? =D”
+            },
+        ]
+    }
+];
+
+
+
+
+
