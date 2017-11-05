d891e450089da253dcfd4f113446e063e0fa7754
raissabarros <raissabarros@alu.ufc.br>
2017-11-05 10:49:37 -0300

    Add files via upload

--- Trabalho 02/trab02.js
+++ Trabalho 02/trab02.js
@@ -2,96 +2,82 @@
  * Created by raissa on 31/10/17.
  */
 
-/*function entrar(){
-    document.getElementsById("conversas").style.display = "block";
-}
-
-var entrar = document.getElementsByClassName("conversas");
-var i;
-
-for (i = 0; i < entrar.length; i++) {
-   entar[i].onclick = function(){
-       this.classList.toggle("active");
-
-       var conversas = this.nextElementSibling;
-
-       if (conversas.style.display){
-           conversas.style.display = block;
-       } else {
-         conversas.style.display = none;
-       }
-   }
-}
-
-*/
-
-
-
-
-
-
-
-
-
 
 let chat = [
     {
-        usuario: “mamae”,
+        usuario: "Mamãe",
         imagem: "imagens/avatar02.png",
         mensagens: [
             {
-                usuario: “mamae”,
-                texto: “Bom dia, Baby! Tudo bem?”
+                usuario: "mamae",
+                texto: "Bom dia, Baby! Tudo bem?"
             },
             {
-                usuario: “raissa”,
-                texto: “Tudo, daí”
+                usuario: "raissa",
+                texto: "Tudo, daí"
             },
             {
-                usuario: “mamae”,
-                texto: “Que bom”
+                usuario: "mamae",
+                texto: "Que bom"
             }
         ]
     },
     {
-        usuario: “filipe”,
+        usuario: "Filipe",
         imagem: "imagens/avatar03.png",
         mensagens: [
             {
-                usuario: “filipe”,
-                texto: “Olarrr!”
+                usuario: "filipe",
+                texto: "Olarrr!"
             },
             {
-                usuario: “raissa”,
-                texto: “olarr”
+                usuario: "raissa",
+                texto: "olarr"
             },
             {
-                usuario: “filipe”,
-                texto: “Te amo! Boa aula!”
+                usuario: "filipe",
+                texto: "Te amo! Boa aula!"
             }
         ]
     },
     {
-        usuario: “marilia”,
+        usuario: "Marília",
         imagem: "imagens/avatar01.png",
         mensagens: [
             {
-                usuario: “raissa”,
-                texto: “Miga, vamo no Submarine?”
+                usuario: "raissa",
+                texto: "Miga, vamo no Submarine?"
             },
             {
-                usuario: “marilia”,
-                texto: “ow, miga, tô sem dinheiro =(”
+                usuario: "marilia",
+                texto: "ow, miga, tô sem dinheiro =("
             },
             {
-                usuario: “raissa”,
-                texto: “ow, bicha, queria ir. Eu pago, bora? =D”
-            },
+                usuario: "raissa",
+                texto: "ow, bicha, queria ir. Eu pago, bora? =D"
+            }
         ]
     }
 ];
 
+let listaamigos = chat.map(function (element){
+    return '<li class="contato"><img src="' + element.imagem + '", class="avatar">' + element.usuario + '</li>';
+}).join('');
 
+document.getElementById('contatinhos').innerHTML = listaamigos;
+
+
+
+let listinha = document.querySelectorAll('.contato');
+for (let i=0; i<listinha.length; i++) {
+    listinha[i].addEventListener("click", function() {
+        document.getElementById('tituloConversa').innerHTML = chat[i].usuario;
+        let mensagi = chat[i].mensagens.map(function (elemento){
+            return elemento.usuario + ': <br/>' + elemento.texto + '<br/><br/>';
+        }).join('');
+        document.getElementById('msg').innerHTML = mensagi;
+    })
+}
 
 
 
